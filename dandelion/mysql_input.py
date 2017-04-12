1580596aea26a043b6d6b7e9a072ae6e07bed581
peter850421 <peter850421@yahoo.com.tw>
2017-04-13 00:21:15 +0800

    mysql

--- dandelion/mysql_input.py
+++ dandelion/mysql_input.py
@@ -3,18 +3,18 @@ import datetime
 def mysql_input(IP,PORT,BOX_ID,CPU_NUM,CPU_LOADING,LOADING_AVG,Memory,DISK):
     mdb = pymysql.connect("localhost","root","elnj4j;3xj4","live_stream_db" )
     cursor = mdb.cursor()
-    time0 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    Time0 = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     sql = "INSERT INTO `system`(`IP`, `PORT`, `BOX_ID`, `CPU_NUM`,\
      `CPU_LOADING`, `LOADING_AVG`, `Memory`, `DISK`, `time`) \
-     VALUES ({IP}, {PORT}, {BOX_ID}, {CPU_NUM}\
-     {CPU_LOADING}, {LOADING_AVG}, {Memory}, {DISK}\'{time0}\')".format(IP=IP,PORT=PORT,
-                                                                    BOX_ID=BOX_ID,
-                                                                    CPU_NUM=CPU_NUM,
-                                                                    CPU_LOADING=CPU_LOADING,
-                                                                    LOADING_AVG=LOADING_AVG,
-                                                                    Memory=Memory,
-                                                                    DISK=DISK,
-                                                                    time0=time0)
+     VALUES ({ip}, {port}, {box_id}, {cpu_num}\
+     {cpu_loading}, {loading_avg}, {memory}, {disk}\'{time0}\')".format(ip=IP,port=PORT,
+                                                                    box_id=BOX_ID,
+                                                                    cpu_num=CPU_NUM,
+                                                                    cpu_loading=CPU_LOADING,
+                                                                    loading_avg=LOADING_AVG,
+                                                                    memory=Memory,
+                                                                    disk=DISK,
+                                                                    time0=Time0)
     try:
         cursor.execute(sql)
         mdb.commit()
