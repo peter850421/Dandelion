759dee47f901d4e3a8b761057f7865e17987db4b
ktshen <bruce20036@gmail.com>
2017-04-18 21:25:27 +0800

    Add file log and edit readme

--- dandelion/logger.py
+++ dandelion/logger.py
@@ -1,12 +1,16 @@
 import logging
 from colorlog import ColoredFormatter
 
+COLOR_FORMAT = "%(log_color)s[%(levelname)s]%(reset)s %(purple)s%(name)s %(processName)s %(reset)s %(bold_blue)s%(funcName)s%(reset)s %(green)s%(asctime)s%(reset)s \n%(message_log_color)s%(message)s"
+
+FORMAT = "%(levelname)s %(name)s %(processName)s %(funcName)s %(asctime)s\n%(message)s"
+
+DATEFMT = '%Y/%m/%d %H:%M:%S'
 
 def get_logger(name, level=logging.DEBUG):
     _formatter = ColoredFormatter(
-        "%(log_color)s[%(levelname)s]%(reset)s %(purple)s%(name)s %(processName)s %(reset)s %(bold_blue)s%("
-        "funcName)s%(reset)s %(green)s%(asctime)s%(reset)s \n%(message_log_color)s%(message)s",
-        datefmt='%Y/%m/%d %H:%M:%S',
+        COLOR_FORMAT,
+        datefmt=DATEFMT,
         reset=True,
         log_colors={
             'DEBUG': 'cyan',
@@ -27,6 +31,10 @@ def get_logger(name, level=logging.DEBUG):
     )
     _handler = logging.StreamHandler()
     _handler.setFormatter(_formatter)
+    logging.basicConfig(filename='dandelion.log', 
+                        level=level, 
+                        format=FORMAT, 
+                        datefmt=DATEFMT)
     logger = logging.getLogger(name)
     logger.addHandler(_handler)
     logger.setLevel(level=level)
